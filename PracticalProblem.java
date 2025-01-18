36e2c743e1db39c6cf3d34aad5403d58fcc6b278
krishna saxena <100754752+krishnaSaxe221@users.noreply.github.com>
2025-01-18 15:03:55 +0530

    Main

---
+++ PracticalProblem.java
@@ -0,0 +1,17 @@
+public class PracticalProblem{
+     public static void main(String[] args){
+           System.out.println("Even no from 1 to 10: ");
+           for(int i = 0; i<= 10; i++){
+                 if(i%2==0){
+                     System.out.println(i + " ");
+}
+}
+
+           System.out.println("Odd numbers from 1 to 10");
+           for(int i = 0 ; i<=10; i++){
+                   if(i%2 !=0){
+             System.out.println(i + " ");
+}
+}
+}
+}
